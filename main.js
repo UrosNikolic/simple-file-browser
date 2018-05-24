7daf24b3c1604d2420fa9d6bbe7586044dd099c1
Uros Nikolic <unikolic94@gmail.com>
2018-05-25 00:09:49 +0200

    simple-file-browser app initial verison

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 800 })
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // uncomment this line in order to open DevTools
+    // mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
---
+++ renderer.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const util = require('util')
+const fs = require('fs')
+
+const lstat = util.promisify(fs.lstat)
+const readdir = util.promisify(fs.readdir)
+const readFile = util.promisify(fs.readFile)
+
+Vue.component('listing', {
+    props: ['item'],
+    template: '<div class="listing-item" @click="clicked(item.name)">{{ item.name }}</div>',
+    methods: {
+        clicked(n) {
+            go(path.format({ dir: app.location, base: n }))
+        }
+    }
+})
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        location: process.cwd(),
+        files: [],
+        tmpFiles: [],
+        image: null,
+        fileContent: null
+    },
+    methods: {
+        up () {
+            go(path.dirname(this.location))
+        },
+        filterCurrentFiles ({ currentTarget: { value }}) {
+           if (!value) {
+               this.files = this.tmpFiles
+           } else {
+               this.files = this.files.filter((file) => file.name.indexOf(value) > -1)
+           }
+        }
+    }
+});
+
+async function go(currentPath) {
+    if(currentPath.endsWith('.bpm') || currentPath.endsWith('.png') || currentPath.endsWith('.gif') || currentPath.endsWith('.jpg')) {
+        app.image = 'file://' + currentPath;
+    } else {
+        app.image = null
+        app.fileContent = null
+
+        try {
+            const stat = await lstat(currentPath)
+
+            if (stat.isDirectory()) {
+                app.location = currentPath
+
+                const files = await readdir(app.location)
+                const a = []
+                for(let i = 0; i < files.length; i++) {
+                    a.push({ id: i, name: files[i] })
+                    app.files = a
+                    app.tmpFiles = a
+                }
+            } else {
+                app.fileContent = await readFile(currentPath, 'utf8')
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
